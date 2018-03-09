4b4f7d24243d93333754e4809fc8ee934fc6473a
verywarmsound <imkarpacz@gmail.com>
2018-03-09 14:39:43 +0100

    Initial commit

---
+++ Scripts/myscript.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+ 
+    "use strict";
+    var wheight = $(window).height(); //get the height of the window
+
+    $('.fullheight').css('height', wheight); //set to window tallness  
+    //replace IMG inside carousels with a background image
+    $('#featured .item img').each(function () {
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({ 'background-image': 'url(' + imgSrc + ')' });
+        $(this).remove();
+    });
+
+    //adjust height of .fullheight elements on window resize
+    $(window).resize(function () {
+        wheight = $(window).height(); //get the height of the window
+        $('.fullheight').css('height', wheight); //set to window tallness  
+    });
+
+})
